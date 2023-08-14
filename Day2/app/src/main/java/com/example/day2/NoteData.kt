d43643300973c7b48c1b4b91073d21b3351d69a8
KorayHamsioglu <87299676+KorayHamsioglu@users.noreply.github.com>
2023-08-14 14:47:13 +0300

    Delete Day2 directory

--- Day2/app/src/main/java/com/example/day2/NoteData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.day2
-
-import com.google.gson.annotations.SerializedName
-
-data class NoteData(@SerializedName("title") var title: String,
-                    @SerializedName("content") var content: String)
